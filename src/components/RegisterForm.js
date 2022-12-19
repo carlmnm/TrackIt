8d40cb3971bdba6c1fe43dbf4b17245cd51356ee
carlmnm <carlossmiguell3@gmail.com>
2022-12-19 00:14:03 -0300

    tela de cadastro concluída

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,69 +2,95 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
+import { ThreeDots } from 'react-loader-spinner'
+
 
 export default function UserSignIn() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [name, setName] = useState("")
     const [image, setImage] = useState("")
+    const [usage, setUsage] = useState(false)
+    const [buttonText, setButtonText] = useState(<p>{"Cadastrar"}</p>)
     const navigate = useNavigate()
 
-    function createAccount (e){
+    function createAccount(e) {
         e.preventDefault()
         const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up"
-        const body = {email, name, image, password}
+        const body = { email, name, image, password }
+        setButtonText(                    
+        <ThreeDots
+            height="80"
+            width="51"
+            radius="9"
+            color="#ffffff"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+        />)
+
 
         const promise = axios.post(URL, body)
         promise.then(res => {
             alert("Cadastro realizado!")
             navigate("/")
+
         })
 
-        promise.catch((err) => alert(err.response.data.message))
+        promise.catch((err) => {
+            alert(err.response.data.message)
+            setUsage(false)
+            setButtonText(<p>{"Cadastrar"}</p>)
+            
+        })
 
     }
 
     return (
         <ContainerForm onSubmit={createAccount}>
-            <form>
-                <Input 
-                type="email" 
-                placeholder="e-mail" 
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                required
+            <form disabled>
+                <Input
+                    type="email"
+                    placeholder="e-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                    disabled={usage}
                 />
 
-                <Input 
-                type="password" 
-                placeholder="senha" 
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                required
+                <Input
+                    type="password"
+                    placeholder="senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                    disabled={usage}
                 />
 
-                <Input 
-                type="text" 
-                placeholder="nome"
-                value={name}
-                onChange={e => setName(e.target.value)}
-                required
+                <Input
+                    type="text"
+                    placeholder="nome"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    required
+                    disabled={usage}
                 />
 
-                <Input 
-                type="url" 
-                placeholder="foto"
-                value={image}
-                onChange={e => setImage(e.target.value)}
-                required
+                <Input
+                    type="url"
+                    placeholder="foto"
+                    value={image}
+                    onChange={e => setImage(e.target.value)}
+                    required
+                    disabled={usage}
                 />
 
-                <ButtonLogin>
-                    <p>
-                        {"Cadastrar"}
-                    </p>
+                <ButtonLogin disabled={usage}>
+                    {buttonText}
+
                 </ButtonLogin>
+
             </form>
         </ContainerForm>
     )
@@ -95,6 +121,9 @@ width: 318px;
 height: 45px;
 background: #52B6FF;
 border-radius: 5px;
+display: flex;
+justify-content: center;
+align-items: center;
 p{
     font-family: 'Lexend Deca';
     font-style: normal;
